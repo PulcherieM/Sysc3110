f2f21e4754596af5d165705feb7cb2ec6a4ca62a
Cherie <pulcheriembaye11@gmail.com>
2025-09-17 22:43:41 -0400

    Added a message for address book in main

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+public class AddressBook {
+
+
+
+
+
+    public void addBuddy(BuddyInfo buddy){
+
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+
+
+}
